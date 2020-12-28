c2c546f3431b90735f52cc8b5ddb5a869415f0c8
Mick Kanchanachayphoom <mick@MacBook-Pro.local>
2020-12-28 22:46:36 +0700

    First commit on Bitcoin alarm project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import useSound from 'use-sound';
+import alarm from './alarm.mp3';
 
 function App() {
+
+  const [btcState, setBtcState] = useState({
+    loading: false,
+    data: null,
+  });
+
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [play] = useSound(alarm);
+
+  useEffect(() => {
+    setBtcState({loading: true})
+    const apiUrl = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(data => {
+        setBtcState({
+          loading: false,
+          data: data,
+        })
+      });
+    setTimeout(() => setCurrentDate(new Date()), 60000);
+
+    if (btcState.data != null && btcState.data.bpi.USD.rate_float < 22800) {
+      play();
+    }
+
+  }, [currentDate]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Block">
+        <h1>Bitcoin Price</h1>
+        {btcState.data != null &&
+          <p>${btcState.data.bpi.USD.rate}</p>
+        }
+        {/* <button onClick={stop}>Click To Stop!</button> */}
+      </div>
     </div>
   );
 }
